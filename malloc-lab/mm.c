b6a3abbe1803c9a3a7561c80efb1387a8e7c9bd6
edwardlee <openmoresome@gmail.com>
2025-10-25 17:39:23 +0900

    2025-10-25

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -23,72 +23,52 @@
  * provide your team information in the following struct.
  ********************************************************/
 team_t team = {
-    /* Team name */
-    "ateam",
-    /* First member's full name */
-    "Harry Bovik",
-    /* First member's email address */
-    "bovik@cs.cmu.edu",
-    /* Second member's full name (leave blank if none) */
-    "",
-    /* Second member's email address (leave blank if none) */
-    ""};
+    "Krafton Jungle 11", /* Team name */
+    "Jongyun Lee", /* First member's full name */
+    "openmoresome@gmail.com", /* First member's email address */
+    "", /* Second member's full name (leave blank if none) */
+    "", /* Second member's email address (leave blank if none) */
+};
 
-/* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
-
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
-
+#define ALIGNMENT 8 /* single word (4) or double word (8) alignment */
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7) /* rounds up to the nearest multiple of ALIGNMENT */
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/*
- * mm_init - initialize the malloc package.
- */
-int mm_init(void)
-{
+
+// mm_init - initialize the malloc package.
+int mm_init(void) {
     return 0;
 }
 
-/*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
-void *mm_malloc(size_t size)
-{
+
+// mm_malloc - Allocate a block by incrementing the brk pointer.
+// Always allocate a block whose size is a multiple of the alignment.
+void *mm_malloc(size_t size) {
     int newsize = ALIGN(size + SIZE_T_SIZE);
     void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-        return NULL;
-    else
-    {
+    if (p == (void *)-1) return NULL;
+    else {
         *(size_t *)p = size;
         return (void *)((char *)p + SIZE_T_SIZE);
     }
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- */
-void mm_free(void *ptr)
-{
+
+// mm_free - Freeing a block does nothing.
+void mm_free(void *ptr) {
 }
 
-/*
- * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
- */
-void *mm_realloc(void *ptr, size_t size)
-{
+
+// mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+void *mm_realloc(void *ptr, size_t size) {
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
 
     newptr = mm_malloc(size);
-    if (newptr == NULL)
-        return NULL;
+    if (newptr == NULL) return NULL;
     copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-        copySize = size;
+    if (size < copySize) copySize = size;
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
